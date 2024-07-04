a977dcd394d2cb3b898ebb4a0ef052fe039536f3
miriamhwork <miriamhwork@hotmail.com>
2024-07-04 10:25:05 -0300

    correção da segunda parte do exercício2

--- exercicio2/src/components/Produto.jsx
+++ exercicio2/src/components/Produto.jsx
@@ -10,8 +10,36 @@ function Produto(props) {
             <p className="valor-desconto">Desconto de {props.desconto}%</p>
             <p className="preco-original">De: R$ {props.preco}</p>
             <p className="preco-desconto">Por: R${contaDesconto}</p>
-        </section>
+        </section>        
     );
 }
 
 export default Produto;
+
+
+// Correção do exercício dia 04/07, não fiz as condicionais
+// function CardProduto(props) {
+//     const valorDesconto = props.precoUnitario - props.precoUnitario * (props.desconto/100);
+
+//     return (
+//         <article className="card-produto">
+//             <p>{props.nome}</p>
+            
+//             {props.desconto > 0 && <p className="grifado">R$ {props.precoUnitario}</p>} 
+//             {/* condicional para aparecer o valor original grifado, se tiver desconto maior que zero */}
+
+//             {props.desconto > 0 ? <h4>R$ {valorDesconto}</h4> : <h4>R$ {props.precoUnitario}</h4> }
+//             {/* ternário para aparecer o valor destacado original ou descontado, se o desconto for maior que zero */}
+
+//             {props.desconto > 0 && <p className="desconto">{props.desconto}% OFF</p>}
+//             {/* condicional para aparecer o valor do desconto em verde só se o valor do desconto for maior que zero */}
+
+//         </article>
+//     );
+// }
+
+// export default CardProduto; 
+
+/* Quando usar o && -> quando tiver uma condição que depende de uma condição */
+/* Quando usar o ? : -> quando tiver duas situações */
+/* Quando quiser colocar a formatação de moeda local automaticamente -> props.precoUnitario.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) */
